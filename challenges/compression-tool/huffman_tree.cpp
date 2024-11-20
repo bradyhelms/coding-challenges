781c2bece61e88a3f1444ea6c2bb1ae0bfec48bc
Brady Helms <bradyhelms@gmail.com>
2024-11-20 08:12:25 -0500

    add huffmancode.cpp

--- challenges/compression-tool/huffman_tree.cpp
+++ challenges/compression-tool/huffman_tree.cpp
@@ -21,7 +21,7 @@ Node* new_node(char ch, int freq, Node* left, Node* right) {
   return node;
 }
 
-struct compare {
+struct comp {
   bool operator()(Node* l, Node* r) {
     return l->freq > r->freq;
   }
@@ -40,4 +40,45 @@ void encode(Node* root, std::string str, std::unordered_map<char, std::string>& 
   encode(root->right, str + "1", huffman);
 }
 
-void decode(Node* root, int *index, std::string str);
+void decode(Node* root, int *index, std::string str) {
+  if (root == nullptr) {
+    return;
+  }
+
+  if (!root->left && !root->right) {
+    std::cout << root->ch;
+    return;
+  }
+
+  index++;
+
+  if (str[index] == '0') {
+    decode(root->left, index, str);
+  } else {
+    decode(root->right, index, str);
+  }
+}
+
+void build_huff_tree(std::unordered_map<char, int> char_freq, std::string text) {
+  std::priority_queue<Node*, std::vector<Node*>, comp> pq;
+
+  for (auto pair : char_freq) {
+    pq.push(new_node(pair.first, pair.second, nullptr, nullptr));
+  }
+
+  while (pq.size() != 1) {
+    Node* left = pq.top();
+    pq.pop();
+
+    Node* right = pq.top();
+    pq.pop();
+
+    int sum = left->freq + right ->freq;
+    pq.push(new_node('\0', sum, left, right));
+  }
+
+  Node* root = pq.top();
+
+  std::unordered_map<char, std::string> huffman_code;
+  encode(root, "", huffman_code);
+}
